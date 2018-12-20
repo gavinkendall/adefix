cee8bec5f9982144968b3f33882acf27238f37ce
Gavin Kendall <gkendall@igloosoftware.com>
2018-12-20 16:12:20 -0500

    1.0.0.0 - A better version of the prototype I wrote last year.

---
+++ adefix/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace adefix
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            ShowMenu();
+        }
+
+        private static void Continue()
+        {
+            Console.WriteLine("\nPress Enter to continue ...");
+            Console.ReadLine();
+        }
+
+        private static void ShowMenu()
+        {
+            ConsoleKey key;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Adobe Digital Editions Fix (v1.0)");
+                Console.WriteLine("Programming by Gavin Kendall");
+                Console.WriteLine("==============================================");
+
+                Console.WriteLine("\nPlease select an option.");
+
+                Console.WriteLine("\n1) Fix issues with the application freezing");
+                Console.WriteLine("2) Fix issues with books that cannot open");
+                Console.WriteLine("3) Fix issues with authorization/de-authorization");
+                Console.WriteLine("4) All of the above");
+
+                Console.WriteLine("\nPress Esc to exit");
+
+                key = Console.ReadKey().Key;
+
+                Fix(key);
+
+            } while (!key.Equals(ConsoleKey.Escape));
+        }
+
+        private static void Fix(ConsoleKey key)
+        {
+            try
+            {
+                switch (key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad1:
+                    case ConsoleKey.NumPad2:
+                        Console.Clear();
+                        Solutions.DeleteAdobeLocalAppDirectorySubfolders();
+                        Continue();
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        Console.Clear();
+                        Solutions.DeleteAdobeAdeptRegistryKey();
+                        Continue();
+                        break;
+
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        Console.Clear();
+                        Solutions.DeleteAdobeLocalAppDirectorySubfolders();
+                        Solutions.DeleteAdobeAdeptRegistryKey();
+                        Continue();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Exception error encountered");
+                Console.WriteLine(ex.ToString());
+
+                Continue();
+                ShowMenu();
+            }
+        }
+    }
+}
---
+++ adefix/Solutions.cs
@@ -0,0 +1,53 @@
+﻿namespace adefix
+{
+    using System;
+    using System.IO;
+    using Microsoft.Win32;
+
+    public static class Solutions
+    {
+        private static readonly string AdobeDigitalEditionsExeSearchPattern = "DigitalEditions.exe*";
+        private static readonly string AdobeSystemsLocalDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Adobe_Systems_Incorporate";
+
+        public static void DeleteAdobeLocalAppDirectorySubfolders()
+        {
+            if (Directory.Exists(AdobeSystemsLocalDataDirectory))
+            {
+                Console.WriteLine($"Searching Adobe local app data directory for \"{AdobeDigitalEditionsExeSearchPattern}\":");
+                Console.WriteLine(AdobeSystemsLocalDataDirectory);
+
+                string[] dirs = Directory.GetDirectories(AdobeSystemsLocalDataDirectory, AdobeDigitalEditionsExeSearchPattern);
+
+                Console.WriteLine($"{dirs.Length} subfolder entries found");
+
+                foreach (string dir in dirs)
+                {
+                    Directory.Delete(dir, true);
+                    Console.WriteLine($"Deleted \"{dir}\"");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Cannot find Adobe local app data directory \"{AdobeSystemsLocalDataDirectory}\"");
+            }
+        }
+
+        public static void DeleteAdobeAdeptRegistryKey()
+        {
+            Console.WriteLine("Finding Adobe Adept registry key");
+
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\Adobe", true);
+
+            if (registryKey != null)
+            {
+                registryKey.DeleteSubKey("Adept\\Device");
+                registryKey.DeleteSubKey("Adept");
+                Console.WriteLine("Adobe Adept registry key deleted");
+            }
+            else
+            {
+                Console.WriteLine("Cannot find Adobe Adept registry key");
+            }
+        }
+    }
+}
